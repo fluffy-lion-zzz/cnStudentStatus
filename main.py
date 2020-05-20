2ecfac4afb3a56c3a0f778152fa67c257f8f069d
Code Nation <codenation@Fluffy-Lion.local>
2020-05-20 10:08:32 +0100

    organised data table with pandas and tested with print

--- main.py
+++ main.py
@@ -35,18 +35,6 @@ pd.DataFrame(names)
 
 print(pd.DataFrame(names))
 
-# colors = ["#A6B91A", "#705746", "#6F35FC", "#F7D02C", "#D685AD",
-#           "#C22E28", "#EE8130", "#A98FF3", "#735797", "#7AC74C",
-#           "#E2BF65", "#96D9D6", "#A8A77A", "#A33EA1", "#F95587",
-#           "#B6A136", "#B7B7CE", "#6390F0"];
-
-#  DA 2AA9BB
-# main CN F5B32F
-# yoda 5DBCD2
-# better green 3ADB79
-# red EB1921
-# silverish DCEBE9
-# black 000000
 hex_colours = ["#3ADB79", "#DCEBE9", "#F5B32F", "#2AA9BB", "#EB1921", "#000000"]
 # d3.schemeSet3
 Chord(matrix, names, wrap_labels=True, colors=hex_colours).to_html()
